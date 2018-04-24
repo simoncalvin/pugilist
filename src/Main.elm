7926f450ab5d2ef20be6bee9e0966694f21dc32e
Simon Calvin <simon.calvin@gmail.com>
2018-04-24 07:44:16 -0500

    first commit

---
+++ src/Main.elm
@@ -0,0 +1,67 @@
+module Main exposing (main)
+
+import Html exposing (Html, section, div, header, h1, text, footer, a, form, input, button, ul, li, p, main_)
+import Html.Attributes exposing (class, type_, maxlength, placeholder, id, href)
+import Html.Events exposing (onInput, onSubmit)
+import Http
+
+type alias Model = 
+    { search: String
+    , results : List String
+    }
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = (Model "" [], Cmd.none)
+        , update = update
+        , view = view
+        , subscriptions = always Sub.none
+        }
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ header []
+            [ h1 [] [ text "puglist: your pull list" ] ]
+        , main_ []
+            [ p [ class "bold" ] [ text "Oh hi," ]
+            , p [] [ text "Tell a title on your list:" ]
+            , form [ onSubmit SubmitSearch ]
+                [ input [ onInput InputSearch, type_ "text", maxlength 100, placeholder "Title!" ]
+                    [ text model.search ]
+                , button [ type_ "submit" ] [ text "Submit" ]
+                ]
+            , section [ class "dreams" ]
+                [ p [] [ text "Which one?" ]
+                , ul [ id "results" ]
+                    (model.results |> List.map (\dream -> li [] [ text dream ]))
+                ]
+            ]
+        , footer []
+            [ a [ href "https://glitch.com" ]
+                [ text "Remix this in Glitch" ]
+            ]
+        ]
+
+type Msg
+    = InputSearch String
+    | SubmitSearch
+    | ReceiveSearch (Result Http.Error (List String))
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        InputSearch value ->
+            ( model, Cmd.none )
+
+        SubmitSearch ->
+            ( model, Cmd.none )
+
+        ReceiveSearch (Ok results) ->
+            ( model, Cmd.none )
+
+        ReceiveSearch (Err error) ->
+            ( model, Cmd.none )
+            
