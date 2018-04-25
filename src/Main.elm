801780cacabe3308bb7b55a586201e5df0914839
Simon Calvin <simon.calvin@gmail.com>
2018-04-24 22:59:04 -0500

    scaffolding

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,14 @@
 module Main exposing (main)
 
-import Html exposing (Html, section, div, header, h1, text, footer, a, form, input, button, ul, li, p, main_)
-import Html.Attributes exposing (class, type_, maxlength, placeholder, id, href)
+import Html exposing (Html, a, button, div, footer, form, h1, header, input, li, main_, p, section, text, ul)
+import Html.Attributes exposing (class, href, id, maxlength, placeholder, type_)
 import Html.Events exposing (onInput, onSubmit)
-import Http
+import Http exposing (get, send)
+import Json.Decode exposing (list, string)
 
-type alias Model = 
-    { search: String
+
+type alias Model =
+    { search : String
     , results : List String
     }
 
@@ -14,20 +16,21 @@ type alias Model =
 main : Program Never Model Msg
 main =
     Html.program
-        { init = (Model "" [], Cmd.none)
+        { init = ( Model "" [], Cmd.none )
         , update = update
         , view = view
         , subscriptions = always Sub.none
         }
 
+
 view : Model -> Html Msg
 view model =
     div []
         [ header []
-            [ h1 [] [ text "puglist: your pull list" ] ]
+            [ h1 [] [ text "pugilist: your pull list" ] ]
         , main_ []
             [ p [ class "bold" ] [ text "Oh hi," ]
-            , p [] [ text "Tell a title on your list:" ]
+            , p [] [ text "Tell me a title on your list:" ]
             , form [ onSubmit SubmitSearch ]
                 [ input [ onInput InputSearch, type_ "text", maxlength 100, placeholder "Title!" ]
                     [ text model.search ]
@@ -45,23 +48,27 @@ view model =
             ]
         ]
 
+
 type Msg
     = InputSearch String
     | SubmitSearch
     | ReceiveSearch (Result Http.Error (List String))
 
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         InputSearch value ->
-            ( model, Cmd.none )
+            ( { model | search = value }, Cmd.none )
 
         SubmitSearch ->
-            ( model, Cmd.none )
+            ( model
+            , Http.get ("/titles?search=" ++ model.search) (list string)
+                |> Http.send ReceiveSearch
+            )
 
         ReceiveSearch (Ok results) ->
-            ( model, Cmd.none )
+            ( { model | results = results }, Cmd.none )
 
         ReceiveSearch (Err error) ->
             ( model, Cmd.none )
-            
